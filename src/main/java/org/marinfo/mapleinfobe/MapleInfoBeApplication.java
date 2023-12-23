c5f3e75388f0c9baab909f8f48f910e0c84a8ca1
오상현 <osh@osanghyeon-ui-MacBookAir.local>
2023-12-23 18:17:09 +0900

    초기세팅: spring boot 프로젝트 생성

---
+++ src/main/java/org/marinfo/mapleinfobe/MapleInfoBeApplication.java
@@ -0,0 +1,13 @@
+package org.marinfo.mapleinfobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleInfoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapleInfoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/marinfo/mapleinfobe/MapleInfoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marinfo.mapleinfobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleInfoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
