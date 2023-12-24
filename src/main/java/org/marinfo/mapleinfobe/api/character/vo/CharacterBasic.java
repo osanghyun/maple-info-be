7a7d5ee0d5c02efbb94190db29f2dfacceefdcd0
오상현 <osh@osanghyeon-ui-MacBookAir.local>
2023-12-24 18:41:17 +0900

    노가다: camel case 반환 설정 추가

--- src/main/java/org/marinfo/mapleinfobe/api/character/vo/CharacterBasic.java
+++ src/main/java/org/marinfo/mapleinfobe/api/character/vo/CharacterBasic.java
@@ -1,8 +1,7 @@
 package org.marinfo.mapleinfobe.api.character.vo;
 
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,18 +9,27 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CharacterBasic {
     private String date;
+    @JsonAlias({"character_name"})
     private String characterName;
+    @JsonAlias({"world_name"})
     private String worldName;
+    @JsonAlias({"character_gender"})
     private String characterGender;
+    @JsonAlias({"character_class"})
     private String characterClass;
+    @JsonAlias({"character_class_level"})
     private String characterClassLevel;
+    @JsonAlias({"character_level"})
     private Long characterLevel;
+    @JsonAlias({"character_exp"})
     private Long characterExp;
+    @JsonAlias({"character_exp_rate"})
     private String characterExpRate;
+    @JsonAlias({"character_guild_name"})
     private String characterGuildName;
+    @JsonAlias({"character_image"})
     private String characterImage;
 
 }
